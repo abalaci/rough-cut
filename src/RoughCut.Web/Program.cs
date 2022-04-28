545bdb5bc643cde041f189bca4cc120e5d82bb82
Andrei Balaci <47945400+abalaci@users.noreply.github.com>
2022-04-28 16:43:05 +0300

    Add Razor Pages template

---
+++ src/RoughCut.Web/Program.cs
@@ -0,0 +1,18 @@
+﻿WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
+WebApplication app = builder.Build();
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapRazorPages();
+
+app.Run();
