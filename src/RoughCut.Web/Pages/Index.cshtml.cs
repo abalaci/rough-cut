b9637ae7993388e64176f544567709d217400491
abalaci <47945400+abalaci@users.noreply.github.com>
2022-05-06 21:18:37 +0300

    Use images on home page

--- src/RoughCut.Web/Pages/Index.cshtml.cs
+++ src/RoughCut.Web/Pages/Index.cshtml.cs
@@ -11,28 +11,28 @@ public class IndexModel : PageModel
         {
             Title = "Dolores no illum magna sanctus et lorem enim te quod accusam duo",
             Description = "Elitr enim consetetur erat eos vel sea sed nonumy elitr lobortis lorem invidunt nisl. Facilisi erat rebum rebum sed est augue et possim nonummy invidunt elitr consequat sed sea accusam nonumy.",
-            ImageUrl = "",
+            ImageUrl = "/assets/img/home-bg.jpg",
             Created = new DateTime(year: 2022, month: 5, day: 3),
         },
         new Article
         {
             Title = "Et delenit dignissim facilisis tempor voluptua",
             Description = "Ea sea sadipscing amet facilisis justo aliquam labore. Invidunt justo elitr nonumy at sed. Vulputate ut voluptua diam id magna sed lobortis sed ut molestie justo amet est. Diam dolore sanctus stet vel et. Augue ut sed sadipscing at dolore justo rebum lorem amet est ut imperdiet ullamcorper diam dolore et blandit dolores.",
-            ImageUrl = "",
+            ImageUrl = "/assets/img/about-bg.jpg",
             Created = new DateTime(year: 2022, month: 5, day: 2),
         },
         new Article
         {
             Title = "Diam nihil diam sanctus ipsum gubergren sit eos ut exerci.",
             Description = "Justo nam amet dolore takimata lorem minim sit sea eirmod ipsum tempor diam no ipsum. Duis velit dolore diam voluptua at esse nulla facilisis exerci gubergren laoreet dolor dolore ea diam. Dolor ut nonumy eu eos. Dolore duis volutpat illum. Sea no sit duo sit voluptua et dignissim. Et eos sed labore.",
-            ImageUrl = "",
+            ImageUrl = "/assets/img/contact-bg.jpg",
             Created = new DateTime(year: 2022, month: 5, day: 1),
         },
         new Article
         {
             Title = "Est erat enim diam labore diam ipsum aliquyam dignissim",
             Description = "Diam lorem rebum dolor consetetur eos sea tempor amet dolor erat dolore iusto sanctus liber duis et laoreet. Rebum accumsan in dolor vero suscipit vel justo dolor. Et sit duo in duo justo vero. Clita at ipsum feugiat eos gubergren et dolor. Facer duo justo et diam amet justo kasd magna nam eirmod.",
-            ImageUrl = "",
+            ImageUrl = "/assets/img/post-bg.jpg",
             Created = new DateTime(year: 2022, month: 5, day: 1),
         }
     };
