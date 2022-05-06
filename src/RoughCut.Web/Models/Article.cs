b5bf3e7edceab891ecec55ca46e9294583bf21f1
abalaci <47945400+abalaci@users.noreply.github.com>
2022-05-06 20:05:01 +0300

    Use Bulma layout

---
+++ src/RoughCut.Web/Models/Article.cs
@@ -0,0 +1,13 @@
+namespace RoughCut.Web.Models
+{
+    public class Article
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public DateTime Created { get; set; }
+    }
+}
