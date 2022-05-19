815547fe2853709431c606f5689531159867858a
Andrei Balaci <47945400+abalaci@users.noreply.github.com>
2022-05-19 22:07:14 +0300

    Add author page (#5)

---
+++ src/RoughCut.Web/Pages/Author.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RoughCut.Web.Models;
+
+namespace RoughCut.Web.Pages
+{
+    public class AuthorModel : PageModel
+    {
+        public IReadOnlyList<Article> Articles { get; set; } = new List<Article>();
+
+        public string Description { get; set; } = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
+
+        public string Id { get; set; } = string.Empty;
+
+        public Uri ImageUrl { get; set; } = new Uri("/", UriKind.Relative);
+
+        public string Name { get; set; } = "Jane Doe";
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
